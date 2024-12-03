fd45d0e587b9934fd5373bd55e4d33877a9c6ed5
mehrdad-ordobadi <mehrdad.ordobadi@gmail.com>
2024-12-02 19:09:01 -0800

    more tf

--- deployment/terraform/modules/eks/launch_template.tf
+++ deployment/terraform/modules/eks/launch_template.tf
@@ -8,15 +8,18 @@ resource "aws_launch_template" "node" {
     ebs {
       volume_size = 20
       volume_type = "gp3"
+      delete_on_termination = true     # Cleanup on instance termination
+      encrypted            = true      # Enable encryption for security
     }
   }
 
   instance_type = "t3.medium"
 
-  metadata_options {
+ metadata_options {
     http_endpoint               = "enabled"
-    http_tokens                 = "required"
+    http_tokens                 = "required"  # IMDSv2 for security
     http_put_response_hop_limit = 1
+    http_protocol_ipv6         = "disabled"  # Disable IPv6 if not needed
   }
 
   monitoring {
@@ -29,6 +32,9 @@ resource "aws_launch_template" "node" {
     resource_type = "instance"
     tags = {
       Name = "${var.project}-node"
+      "k8s.io/cluster-autoscaler/enabled" = "true"
+      "k8s.io/cluster-autoscaler/${var.project}-cluster" = "owned"
+      "kubernetes.io/cluster/${var.project}-cluster" = "owned"
     }
   }
     user_data = base64encode(<<-EOF
@@ -36,4 +42,5 @@ resource "aws_launch_template" "node" {
     /etc/eks/bootstrap.sh ${aws_eks_cluster.main.name}
     EOF
   )
+  update_default_version = true
 }
--- deployment/terraform/modules/eks/sg.tf
+++ deployment/terraform/modules/eks/sg.tf
@@ -51,3 +51,12 @@ resource "aws_security_group_rule" "node_ingress_cluster" {
   source_security_group_id = aws_security_group.cluster.id
   security_group_id        = aws_security_group.node.id
 }
+
+resource "aws_security_group_rule" "node_ingress_lb" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.node.id
+}
